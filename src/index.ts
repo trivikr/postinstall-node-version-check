3c44fff37ceee3481e55d775f50105b6a197fd18
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-07-14 15:02:57 -0700

    feat: add typescript

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-var version = process.version;
+const version = process.version;
 
 if (version && parseInt(version.substring(1, version.indexOf("."))) < 10) {
   console.warn(`The Node.js version ${version} is no longer supported.`);
