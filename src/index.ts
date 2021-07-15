416af44c5487ec05d7710f390dd57895d951d8dc
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2021-07-15 15:49:48 -0700

    test: add tests using jest and ts-jest (#4)

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,4 @@
 #!/usr/bin/env node
 
-const version = process.version;
-
-if (version && parseInt(version.substring(1, version.indexOf("."))) < 10) {
-  process.emitWarning(
-    `The Node.js version ${version} is no longer supported.`,
-    `NodeDeprecationWarning`
-  );
-}
+import { emitWarningIfUnsupportedVersion } from "./emitWarningIfUnsupportedVersion";
+emitWarningIfUnsupportedVersion(process.version);
