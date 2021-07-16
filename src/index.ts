2ba9d2c1a9c8c83cce9d9fc608e2c219f0e826a5
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-07-15 21:51:03 -0700

    wip: add yargs

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 #!/usr/bin/env node
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+const argv = yargs(hideBin(process.argv)).options({
+  supportedVersions: {
+    describe: "The versions of the supported platforms",
+  },
+}).argv;
 
 import { emitWarningIfUnsupportedVersion } from "./emitWarningIfUnsupportedVersion";
 emitWarningIfUnsupportedVersion(process.version);
